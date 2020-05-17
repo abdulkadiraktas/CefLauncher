9e8f5004816b3c151a2202c3eff0eedfe6d3614d
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2020-05-17 13:11:13 +0300

    Create vn.php

---
+++ lang/launcherside/vn.php
@@ -0,0 +1,16 @@
+<?php
+    $langu = array  (
+        "adnotfy" => "Nếu nút đăng nhập không có thì hãy nhấn vào những quảng cáo bất kì. Nếu không thấy quảng cáo, hãy vào lại <a onClick=\"urlOpen('refresh',' ')\"  style=\"color: red !important;\"> here </a>",
+        "tanitim" => "Chạy Intro",
+        "steamname" => "Tên Steam :",
+        "steamUsername" => "User nam steam :",
+        "gta5lock" => "Vị trí tập tin GTA 5 :",
+        "fiveMlock" => "Vị trí tập tin FiveM  : :",
+        "steamlock" => "Vị trí tập tin Steam :",
+        "change" => "Đổi",
+        "login" => "Đăng nhập vào Server",
+        "update" => "Tải phiên bản hiện tại",
+        "settings" => "Cài đặt",
+        "loading" => "Đang tải"
+    );
+?>
